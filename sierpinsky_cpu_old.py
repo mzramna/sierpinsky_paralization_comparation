72c9941423028b905b1bc77a2ceb68f49654870f
Miguel lopes <mzramnadev@gmail.com>
2020-02-28 22:08:25 -0300

    criada classe de modelagem comum e funções de geração de gifs no algoritimo sequencial erro de linha horizontal não sendo desenhada em pontos aleatórios

--- sierpinsky_cpu_old.py
+++ sierpinsky_cpu_old.py
@@ -3,81 +3,11 @@ import time
 import numpy as np
 import cv2
 import multiprocessing as mp
+from modelagem_comum import square,triangle,point
 
 colorir = (0, 255, 255)  # amarelo
 
-
-class point:
-    x = 0
-    y = 0
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def new_zero(self):
-        return point(0, 0)
-
-    def to_array(self):
-        return [self.x, self.y]
-
-    def copy(self, pointn):
-        self.x = pointn.x
-        self.y = pointn.y
-    def debug(self):
-        return ("x:"+str(self.x)+"y:"+str(self.y))
-
-class triangle:
-    # import point
-    left_point = point.new_zero(point)  # ponto esquerdo do triangulo
-    right_point = point.new_zero(point)  # ponto direito do triangulo
-    center_point = point.new_zero(point)  # ponto central do triangulo
-
-    def __init__(self, left: point, right: point, center: point):  # função de preenchimento do triangulo
-        self.left_point = left
-        self.right_point = right
-        self.center_point = center
-
-    def to_array(self):
-        return [self.left_point, self.right_point, self.center_point]
-
-    def to_int_array(self):
-        return [self.left_point.to_array(), self.right_point.to_array(), self.center_point.to_array()]
-    def debug(self):
-        print("left_point:"+str(self.left_point.debug())+"right_point:"+str(self.right_point.debug())+"center_point:"+str(self.center_point.debug()))
-
-class square:
-    # import point
-    inf_esquerdo = point.new_zero(point)  # ponto inferior esquerdo do quadrado
-    inf_direito = point.new_zero(point)  # ponto inferior direito do quadrado
-    sup_esquerdo = point.new_zero(point)  # ponto superior esquerdo do quadrado
-    sup_direito = point.new_zero(point)  # ponto superior direito do quadrado
-
-    def __init__(self, inf_esquerdo_new: point, inf_direito_new: point, sup_esquerdo_new: point,
-                 sup_direito_new: point):  # create_from_points
-        self.inf_esquerdo = inf_esquerdo_new
-        self.inf_direito = inf_direito_new
-        self.sup_esquerdo = sup_esquerdo_new
-        self.sup_direito = sup_direito_new
-
-    def create_from_size(size):  # create from size
-        novo_quadrado = square(point(0, 0), point(size, 0), point(0, size), point(size, size))
-        novo_quadrado.debug()
-        return novo_quadrado
-
-    def to_array(self):  # get all the points from the square
-        return [self.inf_esquerdo, self.inf_direito, self.sup_esquerdo, self.sup_direito]
-
-    def to_int_array(self):  # get all values from each point into square
-        return [self.inf_esquerdo.to_array(), self.inf_direito.to_array(), self.sup_esquerdo.to_array(),
-                self.sup_direito.to_array()]
-    def debug(self):
-        print("sup_esquerdo"+str(self.sup_esquerdo.debug())+"sup_direito"+str(self.sup_direito.debug())+"inf_esquerdo"+str(self.inf_esquerdo.debug())+"inf_direito"+str(self.inf_direito.debug()))
-
-
 class sierpinski_sequencial:  # class to work with the sierpinski triangle fractal
-    def __init__(self):
-        self.pool = mp.Pool(mp.cpu_count())
     def sierpinski_triangles_creator(self,triangle_original: triangle):  # primeira posicao=x,segunda posicao = y
         [left_point, right_point, center_point] = [triangle_original.left_point, triangle_original.right_point,
                                                    triangle_original.center_point]
@@ -173,8 +103,8 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
     def show_img(self, size, depth):  # mostra imagem a partir do algoritimo de
         imagem_branco = self.generate_baseimage(size)
         print("imagem_branco")
-        list_triangles = self.create_depths_from_triangle(self.get_triangle_from_square(square.create_from_size(size)),depth)
-
+        list_triangles = self.create_depths_from_triangle(self.get_triangle_from_square(square.create_from_size(size)),
+                                                          depth)
         print("lista gerada")
         imagem = self.draw_all_triangles_from_list( imagem_branco, list_triangles)
         print("imagem pronta")
--- sierpinsky_sequencial.py
+++ sierpinsky_sequencial.py
@@ -1,78 +1,30 @@
 import time
 
+import imageio as imageio
 import numpy as np
 import cv2
-
+from modelagem_comum import square,triangle,point
 colorir = (0, 255, 255)  # amarelo
 
 
-class point:
-    x = 0
-    y = 0
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def new_zero(self):
-        return point(0, 0)
-
-    def to_array(self):
-        return [self.x, self.y]
-
-    def copy(self, pointn):
-        self.x = pointn.x
-        self.y = pointn.y
-    def debug(self):
-        return ("x:"+str(self.x)+"y:"+str(self.y))
-
-class triangle:
-    # import point
-    left_point = point.new_zero(point)  # ponto esquerdo do triangulo
-    right_point = point.new_zero(point)  # ponto direito do triangulo
-    center_point = point.new_zero(point)  # ponto central do triangulo
-
-    def __init__(self, left: point, right: point, center: point):  # função de preenchimento do triangulo
-        self.left_point = left
-        self.right_point = right
-        self.center_point = center
-
-    def to_array(self):
-        return [self.left_point, self.right_point, self.center_point]
-
-    def to_int_array(self):
-        return [self.left_point.to_array(), self.right_point.to_array(), self.center_point.to_array()]
-    def debug(self):
-        print("left_point:"+str(self.left_point.debug())+"right_point:"+str(self.right_point.debug())+"center_point:"+str(self.center_point.debug()))
-
-class square:
-    # import point
-    inf_esquerdo = point.new_zero(point)  # ponto inferior esquerdo do quadrado
-    inf_direito = point.new_zero(point)  # ponto inferior direito do quadrado
-    sup_esquerdo = point.new_zero(point)  # ponto superior esquerdo do quadrado
-    sup_direito = point.new_zero(point)  # ponto superior direito do quadrado
-
-    def __init__(self, inf_esquerdo_new: point, inf_direito_new: point, sup_esquerdo_new: point,
-                 sup_direito_new: point):  # create_from_points
-        self.inf_esquerdo = inf_esquerdo_new
-        self.inf_direito = inf_direito_new
-        self.sup_esquerdo = sup_esquerdo_new
-        self.sup_direito = sup_direito_new
-
-    def create_from_size(size):  # create from size
-        novo_quadrado = square(point(0, 0), point(size, 0), point(0, size), point(size, size))
-        novo_quadrado.debug()
-        return novo_quadrado
-
-    def to_array(self):  # get all the points from the square
-        return [self.inf_esquerdo, self.inf_direito, self.sup_esquerdo, self.sup_direito]
-
-    def to_int_array(self):  # get all values from each point into square
-        return [self.inf_esquerdo.to_array(), self.inf_direito.to_array(), self.sup_esquerdo.to_array(),
-                self.sup_direito.to_array()]
-    def debug(self):
-        print("sup_esquerdo"+str(self.sup_esquerdo.debug())+"sup_direito"+str(self.sup_direito.debug())+"inf_esquerdo"+str(self.inf_esquerdo.debug())+"inf_direito"+str(self.inf_direito.debug()))
-
+def create_gif(tamanho,profundidade_max,frames_renderizador=None,name=None,duration=None):
+    frames = []
+    sierpinski=sierpinski_sequencial()
+    if duration==None:
+        duration=0.4
+    if frames_renderizador==None:
+        for profundidade in range(0, profundidade_max):
+            frames.append(sierpinski.draw_all_triangles_from_list(sierpinski.generate_baseimage(tamanho),
+                                                                  sierpinski.create_depths_from_triangle(
+                                                                      sierpinski.get_triangle_from_square(
+                                                                          square.create_from_size(tamanho)),
+                                                                      profundidade)).astype(np.uint8))
+    else:
+        frames=frames_renderizador
+    if name==None:
+        name="gif_automatica_0_"+str(profundidade_max)+"_delay"+str(delay)
+    imageio.mimsave(name + ".gif", frames,format='GIF', duration=duration)
+    print("arquivo "+name+" criado com sucesso")
 
 class sierpinski_sequencial:  # class to work with the sierpinski triangle fractal
     def sierpinski_triangles_creator(self,triangle_original: triangle):  # primeira posicao=x,segunda posicao = y
@@ -218,10 +170,9 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
             print("\n\n")
             loop += 1
 
-
 # para testar a exibição inicial
-tamanho=5000
-profundidade=6
+tamanho=10000
+profundidade_max=7
 sierpinski=sierpinski_sequencial()
 #cv2.imshow("test", sierpinski.draw_triangle(sierpinski.generate_baseimage(tamanho),sierpinski.get_triangle_from_square(square.create_from_size(tamanho))))  # working
 #cv2.waitKey()
@@ -233,5 +184,15 @@ sierpinski=sierpinski_sequencial()
 #        print(j)
 #    print("fim")
 # sierpinski_sequencial.debug_triangle_list_generate_son(new_triangles)#working
-for profundidade in range(0,11):
-    sierpinski.save_img("sierpinsky_"+str(profundidade),tamanho, profundidade,mostrar=False)  # testing
+# for profundidade in range(0,11):
+#     sierpinski.save_img("sierpinsky_"+str(profundidade),tamanho, profundidade,mostrar=False)  # testing
+frames=[]
+for profundidade in range(0, profundidade_max):
+    frames.append(sierpinski.draw_all_triangles_from_list(sierpinski.generate_baseimage(tamanho),
+                                                          sierpinski.create_depths_from_triangle(
+                                                              sierpinski.get_triangle_from_square(
+                                                                  square.create_from_size(tamanho)),
+                                                              profundidade)).astype(np.uint8))
+for i in range(4,11):
+    delay=0+i/10
+    create_gif(tamanho=tamanho,profundidade_max=profundidade_max,duration=delay,name="gif_automatica_0_"+str(profundidade_max)+"_delay"+str(delay),frames_renderizador=frames)
