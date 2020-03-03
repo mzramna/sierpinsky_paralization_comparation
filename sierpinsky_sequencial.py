edc85198b7fe7ca6bbc229ffff0883c96d4e9631
Miguel lopes <mzramnadev@gmail.com>
2020-03-03 16:01:04 -0300

    criada documentação de sierpinsky_sequencial.py criada função de geração de gif a partir de diversos fractais gerados,a gif será a criação de todas as camadas possiveis até a profundidade dada

--- sierpinsky_sequencial.py
+++ sierpinsky_sequencial.py
@@ -7,27 +7,17 @@ from modelagem_comum import square,triangle,point
 colorir = (0, 255, 255)  # amarelo
 
 
-def create_gif(tamanho,profundidade_max,frames_renderizador=None,name=None,duration=None):
-    frames = []
-    sierpinski=sierpinski_sequencial()
-    if duration==None:
-        duration=0.4
-    if frames_renderizador==None:
-        for profundidade in range(0, profundidade_max):
-            frames.append(sierpinski.draw_all_triangles_from_list(sierpinski.generate_baseimage(tamanho),
-                                                                  sierpinski.create_depths_from_triangle(
-                                                                      sierpinski.get_triangle_from_square(
-                                                                          square.create_from_size(tamanho)),
-                                                                      profundidade)).astype(np.uint8))
-    else:
-        frames=frames_renderizador
-    if name==None:
-        name="gif_automatica_0_"+str(profundidade_max)+"_delay"+str(delay)
-    imageio.mimsave(name + ".gif", frames,format='GIF', duration=duration)
-    print("arquivo "+name+" criado com sucesso")
+
 
 class sierpinski_sequencial:  # class to work with the sierpinski triangle fractal
+    """classe onde sera executado o processamento do fractal de sierpinsky"""
     def sierpinski_triangles_creator(self,triangle_original: triangle):  # primeira posicao=x,segunda posicao = y
+        """
+        função que gera os triangulos filhos de um triangulo dado posteriormente,el gera mais uma camada da profundidade de pontos,apenas retorno um array com os 3 triangulos resultantes,devido ao modo de funcionamento
+        do fractal só com isso é possivel de se criar o triangulo que o originou
+        :param triangle_original: um triangle
+        :return: array de  3 triangle
+        """
         [left_point, right_point, center_point] = [triangle_original.left_point, triangle_original.right_point,
                                                    triangle_original.center_point]
         largura_total = (right_point.x - left_point.x)
@@ -58,6 +48,12 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
         return [triangle_1, triangle_2, triangle_3]
 
     def draw_triangle(self,img, triangle_1: triangle):  # draw the triangle into the input image and return the ploted image
+        """
+        desenha um triangulo na imagem base passada para a funcao
+        :param img: imagem base em cima da qual será aplicado o fractal
+        :param triangle_1: desenha o triangulo dentro da imagem,usando os dados da classe triangle para isso
+        :return: imagem com triangulo inserido nela
+        """
         new_tri = np.array(
             [triangle_1.left_point.to_array(), triangle_1.right_point.to_array(), triangle_1.center_point.to_array(),
              triangle_1.left_point.to_array()], np.int32)
@@ -66,11 +62,22 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
         return img
 
     def draw_all_triangles_from_list(self, img, triangle_list):
+        """
+        desenha todos os triangulos de uma lista de triangulos na imagem dada
+        :param img: imagem base em cima da qual será aplicado o fractal
+        :param triangle_list: lista de objetos da classe triangle
+        :return: imagem com os triangulos desenhados
+        """
         for i in triangle_list:
             img = self.draw_triangle(img, i)
         return img
 
     def add_depth_from_triangle(self, triangle_list):  # add three triangles inside each triangle from list
+        """
+        adiciona uma nova camada de profundidade ao fractal ao chamar a função sierpinski_triangles_creator para cada triangulo de um array de triangulos
+        :param triangle_list:
+        :return: novo array com os novos triangulos
+        """
         new_triangle_list = []
         for triangle in triangle_list:
             # print(triangle.to_int_array())
@@ -82,6 +89,12 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
         return new_triangle_list
 
     def create_depths_from_size(self, size, depth):
+        """
+        cria o fractal calculando automaticamente todos os triangulos a partir das dimensões e da profundidade desejada
+        :param size: dimensões da imagem de saida,que sera um quadrado
+        :param depth: quantidade de camadas de proifundidade do fractal
+        :return: lista de triangulos calculados a partir dos parametros passados
+        """
         triangle_list = self.sierpinski_triangles_creator(self.get_triangle_from_square(square.create_from_size(size)))
         for i in range(0, depth):
             triangle_list = self.add_depth_triangle(triangle_list)
@@ -89,27 +102,54 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
         return triangle_list
 
     def create_depths_from_triangle(self, triangle: triangle, depth):
+        """
+        cria as camadas de profundidade do triangulo,gerando a quantidade de camas igual a variavel depth
+        :param triangle: objeto primario do tipo triangle
+        :param depth: numero de camadas de profundidade
+        :return: array de triangulos novos gerados a partir de um triangulo dado
+        """
         triangle_list = self.sierpinski_triangles_creator(triangle)
         for i in range(0, depth):
             triangle_list = self.add_depth_from_triangle(triangle_list)
         return triangle_list
 
     def generate_baseimage(self,size):
+        """
+        gera uma nova imagem em branco de formato quadrado e lados iguais a dimensão passada
+        :param size: tamanho do lado do quadrado
+        :return: uma nova imagem em branco
+        """
         new_img = np.ones((size, size, 3))
         return new_img
 
     def get_triangle_from_square(self,square: square):
+        """
+        gera um triangulo a partir de um quadrado
+        :param square: quadrado do qual um triangulo deve ser gerado
+        :return: um triangulo isóceles com lado igual ao lado do quadrado
+        """
         new_triangle = triangle(square.inf_esquerdo, square.inf_direito,
                                 point((square.sup_esquerdo.x + square.sup_direito.x) / 2, (square.sup_esquerdo.y)))
         return new_triangle
 
     def get_square_from_triangle(self,triangle: triangle):
+        """
+        gera um quadrado a partir de um triangulo isóceles com lado igual ao lado do triangulo
+        :param triangle: triangulo isoceles
+        :return: quadrado
+        """
         new_square = square(triangle.left_point, triangle.right_point,
                             point(triangle.left_point.x, triangle.center_point.y),
                             point(triangle.right_point.x, triangle.center_point.y))
         return new_square
 
     def show_img_debug(self, size, depth):  # mostra imagem a partir do algoritimo de
+        """
+        mostra a imagem com dados de debug
+        :param size: tamanho da imagem que será gerada usando a funcao generate_baseimage
+        :param depth: profundidade do fractal usado para debug
+        :return: print de debug e imagem nova com fractal
+        """
         list_triangles = self.create_depths_from_triangle(self.get_triangle_from_square(square.create_from_size(size)),
                                                           depth)
         self.debug_triangle_list(list_triangles)
@@ -120,6 +160,12 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
         cv2.imshow("test", imagem)  # working parcially
 
     def show_img(self, size, depth):  # mostra imagem a partir do algoritimo de
+        """
+        mostra imagem fractal gerada a partir de um quadrado de lado passado pelo parametro e profundidade igual a variavel depth
+        :param size: lado da imagem de saida
+        :param depth: profundidade da imagem gerada
+        :return:mostra a imagem gerada
+        """
         imagem_branco = self.generate_baseimage(size)
         print("imagem_branco")
         list_triangles = self.create_depths_from_triangle(self.get_triangle_from_square(square.create_from_size(size)),
@@ -131,6 +177,14 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
         cv2.waitKey()
 
     def save_img(self, name, size, depth,mostrar=True):  # mostra imagem a partir do algoritimo de
+        """
+        gera uma imagem e salva
+        :param name:nome do arquivo de saida,será do tipo jpg
+        :param size: tamanho do lado da imagem
+        :param depth: profundidade do fractal
+        :param mostrar:se deve exibir a imagem após gerar
+        :return:arquivo de saida com o fractal gerado
+        """
         imagem_branco = self.generate_baseimage(size)
         print("imagem_branco")
         list_triangles = self.create_depths_from_triangle(self.get_triangle_from_square(square.create_from_size(size)),
@@ -145,11 +199,19 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
             cv2.waitKey()
 
     def debug_triangle(self,triangulo: triangle):
-        for j in triangulo.to_int_array():
-            print(j)
-        print("fim")
+        """
+        exibe os dados de um triangulo para debug
+        :param triangulo: triangulo que será debugado
+        :return:debug do triangulo
+        """
+        triangulo.debug()
 
     def debug_triangle_list(self, triangle_list: triangle):
+        """
+        debuga todos os triangulos de uma lista de triangulos
+        :param triangle_list: lista dos triangulos que será debugada
+        :return:print de todos os dados do triangulo
+        """
         loop = 1
         for i in triangle_list:
             print("triangulo " + str(loop))
@@ -157,6 +219,11 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
             loop += 1
 
     def debug_triangle_list_generate_son(self, triangle_list: triangle):
+        """
+        debuga todos os triangulos gerados usango o sierpinski_triangles_creator
+        :param triangle_list: lista dos triangulos originais
+        :return: debug dos filhos criados
+        """
         loop = 1
         for i in triangle_list:
             print("triangulo " + str(loop))
@@ -170,6 +237,33 @@ class sierpinski_sequencial:  # class to work with the sierpinski triangle fract
             print("\n\n")
             loop += 1
 
+    def create_gif(self,tamanho, profundidade_max, frames_renderizador=None, name=None, duration=None):
+        """
+        "cria o gif a partir da execução da função draw all triangles from list
+        :param tamanho: dimensoes do quadrado de tamanho do gif
+        :param profundiade_max: total de vezes que a recursão deve ser executada
+        :param frames_renderizador:podem ser passados todos os frames do gif usando o array , util quando a função de gif for ser usada multiplas vezes para economizar processamento ou se as dimensões nao forem em quadrado
+        :param name:nome do arquivo gif que sera salvo
+        :param duration: tempo que cada frame sera exibido , dado em segundos
+        :return: arquivo gif salvo no lugar definido pelo parametro name
+        """
+        frames = []
+        sierpinski = sierpinski_sequencial()
+        if duration == None:
+            duration = 0.4
+        if frames_renderizador == None:
+            for profundidade in range(0, profundidade_max):
+                frames.append(self.draw_all_triangles_from_list(self.generate_baseimage(tamanho),
+                                                                      self.create_depths_from_triangle(
+                                                                          self.get_triangle_from_square(
+                                                                              square.create_from_size(tamanho)),
+                                                                          profundidade)).astype(np.uint8))
+        else:
+            frames = frames_renderizador
+        if name == None:
+            name = "gif_automatica_0_" + str(profundidade_max) + "_delay" + str(delay)
+        imageio.mimsave(name + ".gif", frames, format='GIF', duration=duration)
+        print("arquivo " + name + " criado com sucesso")
 # para testar a exibição inicial
 tamanho=10000
 profundidade_max=7
@@ -195,4 +289,4 @@ for profundidade in range(0, profundidade_max):
                                                               profundidade)).astype(np.uint8))
 for i in range(4,11):
     delay=0+i/10
-    create_gif(tamanho=tamanho,profundidade_max=profundidade_max,duration=delay,name="gif_automatica_0_"+str(profundidade_max)+"_delay"+str(delay),frames_renderizador=frames)
+    sierpinski.create_gif(tamanho=tamanho,profundidade_max=profundidade_max,duration=delay,name="gif_automatica_0_"+str(profundidade_max)+"_delay"+str(delay),frames_renderizador=frames)
